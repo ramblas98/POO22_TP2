2e648dc1d985cb434f6b3e29593ba6015b3717ca
profeblas <profeblas@localhost>
2022-04-06 18:52:47 -0300

    Agrego saludo desde alumno

--- TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
@@ -14,7 +14,7 @@ public class TP2_EJEMPLO1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola desde la rama Alumno.");
         
     }
     
