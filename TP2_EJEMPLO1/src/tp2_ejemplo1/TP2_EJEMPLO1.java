8f2a82989f1b63ea504d5af674c23a0f7fc485ac
alumno <alumno@pcss01>
2022-04-11 17:50:50 -0300

    main metodo calcular

--- TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp2_ejemplo1;
-
+import java.util.Scanner;
 /**
  *
  * @author profeblas
@@ -15,7 +15,22 @@ public class TP2_EJEMPLO1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner (System.in);
         
+        System.out.print("ingrese la cantidad de empleado");
+        int n = teclado.nextInt(); 
+        Empleado v [];
+        v = new Empleado[n];
+        for (int i =0 ;i<n;i++){
+            v[i]=new Empleado();
+            System.out.print("nombre");
+            v[i].setNombre(teclado.next());
+            System.out.print("horas");
+            v[i].setHoras(teclado.nextInt());
+            System.out.print("valor");
+            v[i].setValor(teclado.nextDouble());
+            System.out.println("sueldo:"+v[i].calcularsueldo());
+        }
     }
     
 }
