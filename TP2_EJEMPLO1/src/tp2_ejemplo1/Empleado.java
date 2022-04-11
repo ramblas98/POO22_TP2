f30ada015249bbcacd99dc9cb8f18c256d810ec4
alumno <alumno@PCSS04>
2022-04-11 17:48:52 -0300

    CREACION DE CLASE

--- TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
@@ -9,5 +9,52 @@ package tp2_ejemplo1;
  * @author profeblas
  */
 public class Empleado {
+  private String nombre;
+  private int horas;
+  private double valor;
+
+  public double calcularSueldo (){
+  
+      return (this.horas*this.valor);
+  
+  }
+ //contructor
+    public Empleado(String nombre, int horas, double valor) {
+        this.nombre = nombre;
+        this.horas = horas;
+        this.valor = valor;
+    }
+//getter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public double getValor() {
+        return valor;
+    }
     
+
+  
+  //setter
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+//constructor sin parametro
+    public Empleado() {
+    }
+    
+  
 }
