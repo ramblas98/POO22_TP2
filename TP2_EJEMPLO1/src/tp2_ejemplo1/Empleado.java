7e0aa75ec8c1427844f79593d9732aaa2008d4a3
alumno <alumno@pcss01>
2022-04-11 17:17:16 -0300

    programe clase empleado

--- TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
@@ -9,5 +9,50 @@ package tp2_ejemplo1;
  * @author profeblas
  */
 public class Empleado {
+    private     
+            String nombre ;
+    private
+            int horas ;
+    private 
+            double valor ;
+
+    public Empleado() {
+        
+    }
+
+    public Empleado(String nombre, int horas, double valor) {
+        this.nombre = nombre;
+        this.horas = horas;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public double calcularsueldo(){
+        return this.horas*this.valor ;
+                
+    }
     
 }
