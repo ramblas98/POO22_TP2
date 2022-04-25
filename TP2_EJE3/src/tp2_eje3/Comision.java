f5e5076065fec2829358d6d2d8a7cf2c91e54580
alumno <alumno@PCSS04>
2022-04-25 17:16:20 -0300

    test eje 3 tp2

---
+++ TP2_EJE3/src/tp2_eje3/Comision.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_eje3;
+import java.util.Scanner;
+/**
+ *
+ * @author alumno
+ */
+public class Comision {
+    private int totalClases;
+    private Estudiante[] listaEstudiantes;
+    private int totalEstudiante;
+Scanner teclado = new Scanner(System.in);
+    public Comision() {
+    }
+
+    public Comision(int totalClases, Estudiante[] listaEstudiantes, int totalEstudiante) {
+        this.totalClases = totalClases;
+        this.listaEstudiantes = listaEstudiantes;
+        this.totalEstudiante = totalEstudiante;
+    }
+
+    public int getTotalClases() {
+        return totalClases;
+    }
+
+    public Estudiante[] getListaEstudiantes() {
+        return listaEstudiantes;
+    }
+
+    public int getTotalEstudiante() {
+        return totalEstudiante;
+    }
+
+    public void setTotalClases(int totalClases) {
+        this.totalClases = totalClases;
+    }
+
+    public void setListaEstudiantes(Estudiante[] listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
+    }
+
+    public void setTotalEstudiante(int totalEstudiante) {
+        this.totalEstudiante = totalEstudiante;
+    }
+   /* public void Comision(){
+    
+    }*/
+    public void comision(int totalClases){
+    
+    }
+   public void agregarEstudiante(){
+   
+   
+   }
+    
+
+public void agregarEstudiante(String apellido,String nombre,int lu, String email){
+
+
+}
+public void mostrarLista(){
+
+}
+
+private void ordenarLista(){
+}
+public Estudiante buscarEstudiante(int lu){
+
+}
+public void mostrarRegulares(){}
+
+public void regularizarEstudiantes(){
+
+}
+}
