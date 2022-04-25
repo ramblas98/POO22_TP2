39b3e5adedfebfc31b6ba55336402d0e7e3bb1fd
Matias <Matias@LAPTOP-PVUVC0VL>
2022-04-25 16:03:29 -0300

    continuar

--- TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/Empleado.java
@@ -9,5 +9,43 @@ package tp2_ejemplo1;
  * @author profeblas
  */
 public class Empleado {
+    private String nombre;
+    private int horas;
+    private double valor;
+    
+    Empleado(String nombre, int horas, double valor){
+        setNombre(nombre);
+        setHoras(horas);
+        setValor(valor);
+    }
+    
+    public void setNombre(String n){
+        nombre = n;
+    }
+    
+    public void setHoras(int h){
+        horas = h;
+    }
+    
+    public void setValor(double v){
+        valor = v;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public int getHoras(){
+        return horas;
+    }
+    
+    public double getValor(){
+        return valor;
+    }
+    
+    public double calcularSueldo(){
+        return horas*valor;
+    }
+    
     
 }
--- TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
+++ TP2_EJEMPLO1/src/tp2_ejemplo1/TP2_EJEMPLO1.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp2_ejemplo1;
-
+ import java.util.Scanner;
 /**
  *
  * @author profeblas
@@ -13,8 +13,32 @@ public class TP2_EJEMPLO1 {
     /**
      * @param args the command line arguments
      */
+   
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        Empleado v[]; String nombre; int horas; double valor;
+        
+        System.out.println("Cantidad de empleados: ");
+        int n = teclado.nextInt();
+        v = new Empleado[n];
+        
+        for(i=0;i<n;i++){
+            v[i] = new Empleado();
+            System.out.println("Nombre del Empleado: ");
+            //nombre = teclado.nextLine();
+            v[i].setNombre(teclado.next());
+            System.out.println("Horas trabajadas: ");
+            horas = teclado.nextInt();
+            v[i].setHoras(horas);
+            System.out.println("Valor de horas trabajadas: ");
+            valor = teclado.nextDouble();
+            v[i].setValor(valor);
+            System.out.println("El sueldo es: " + v[i].calcularSueldo());
+        }
+        
+        
         
     }
     
