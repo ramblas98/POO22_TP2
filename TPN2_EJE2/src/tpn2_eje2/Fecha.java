bb03f62ffd4d6d4973c219fcac7db6401a1ccf95
alumno <alumno@PCSS04>
2022-04-25 16:09:50 -0300

    reubicacion

---
+++ TPN2_EJE2/src/tpn2_eje2/Fecha.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpn2_eje2;
+
+/**
+ *
+ * @author alumno
+ */
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha() {
+    }
+
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+    
+    
+    public void reiniciar(int d, int m, int a){
+    this.dia=d;
+    this.mes=m;
+    this.anio=a;
+        
+    public void adelantar(int d){
+     this.setDia(this.dia+d);
+    
+    }
+    public boolean valida(){}
+    
+    }
+public void normalizar(){
+
+
+}
+public void dias(int m){
+
+}
+public void implimir(){
+
+}
+public boolean bisiesto(int a){
+
+}
+}
