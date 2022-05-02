3a6da19ec9e7aca45b1e54d776631b3a76904317
alumno <alumno@PCSS04>
2022-05-02 17:21:23 -0300

    vededor

---
+++ TP2_EJE5/src/tp2_eje5/Vendedor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_eje5;
+
+/**
+ *
+ * @author alumno
+ */
+public class Vendedor {
+private int legajo;
+private String zona;
+private double ventasM[];
+
+    public Vendedor() {
+    }
+//constructor
+    public Vendedor(int legajo, String zona, String nombre, int dni) {
+        this.legajo = legajo;
+        this.zona = zona;
+        this.nonbre=nombre;
+        this.dni=dni;
+        
+    }
+// set
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+public 
+
+
+
+
+}
