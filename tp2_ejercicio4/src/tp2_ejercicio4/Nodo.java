9a967a8fbf84d8af6a92ae7a809d4da00de267d7
Matias <Matias@LAPTOP-PVUVC0VL>
2022-05-02 15:48:41 -0300

    ejercicio 4

---
+++ tp2_ejercicio4/src/tp2_ejercicio4/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_ejercicio4;
+
+/**
+ *
+ * @author Matias
+ */
+public class Nodo {
+    private int dato;
+    private Nodo sgte;
+    
+    
+    
+    
+}
